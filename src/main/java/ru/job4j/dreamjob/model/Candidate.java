c5edb50ae1aab8ea156a0066195894d2e35c48f2
voldomarich <vbekasov1987@gmail.com>
2024-07-28 00:53:33 +0300

    5. Список кандидатов [#504842 #506929]

---
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -0,0 +1,64 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Candidate {
+
+    private int id;
+    private String title;
+    private String description;
+    private final LocalDateTime creationDate;
+
+    public Candidate(int id, String title, String description, LocalDateTime creationDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.creationDate = creationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -0,0 +1,67 @@
+package ru.job4j.dreamjob.repository;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryCandidateRepository implements CandidateRepository {
+
+    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Candidate> candidates = new HashMap<>();
+
+    private MemoryCandidateRepository() {
+        save(new Candidate(0, "Ivan Brovkin", "для прохождения практики",
+                LocalDateTime.of(2024, Month.APRIL, 11, 11, 11, 11)));
+        save(new Candidate(0, "Matvei Sinicin", "джуниор",
+                LocalDateTime.of(2024, Month.MAY, 15, 12, 33, 29)));
+        save(new Candidate(0, "Leonid Khaibullin", "крепкий мидл после job4j",
+                LocalDateTime.of(2024, Month.JUNE, 28, 9, 48, 14)));
+        save(new Candidate(0, "Pavel Yarsev", "опыт мидлом не менее 2 лет",
+                LocalDateTime.of(2024, Month.JULY, 25, 8, 20, 35)));
+        save(new Candidate(0, "Reso Nashvili", "имеет скилы синьора",
+                LocalDateTime.of(2024, Month.JULY, 1, 9, 0, 3)));
+        save(new Candidate(0, "Petr Arsentiev", "тим лид",
+                LocalDateTime.of(2024, Month.JULY, 22, 12, 12, 25)));
+    }
+
+    public static MemoryCandidateRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Candidate save(Candidate candidate) {
+        candidate.setId(nextId++);
+        candidates.put(candidate.getId(), candidate);
+        return candidate;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        candidates.remove(id);
+    }
+
+    @Override
+    public boolean update(Candidate candidate) {
+        return candidates.computeIfPresent(candidate.getId(),
+                (id, oldCandidate) -> new Candidate(oldCandidate.getId(), candidate.getTitle(),
+                        candidate.getDescription(), candidate.getCreationDate())) != null;
+    }
+
+    @Override
+    public Optional<Candidate> findById(int id) {
+        return Optional.ofNullable(candidates.get(id));
+    }
+
+    @Override
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
